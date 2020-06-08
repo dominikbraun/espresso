a5dd9ae65fa51632e7bd7c805d600b303ec15848
Dominik Braun <Dominik.Braun@nbsp.de>
2020-06-08 09:56:56 +0200

    removed Content model

--- model/content.go
+++ model/content.go
@@ -4,17 +4,6 @@ package model
 
 import "time"
 
-// Content represents general-purpose content like an 'About Me' page
-// or any other types of content that can't be categorized precisely.
-type Content struct {
-	ID           string
-	Title        string
-	RelatedLinks []string
-	Related      []Article
-	Description  string
-	Content      string
-}
-
 // Article represents an editorial article like a magazine article or
 // blog post. An article consists of metadata and its actual content.
 type Article struct {
