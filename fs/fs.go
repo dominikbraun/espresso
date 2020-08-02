b8dcf930de5456731ea0f598ddcf13654674e5f3
Dominik Braun <dominik.braun@nbsp.de>
2020-08-02 22:54:42 +0200

    Add documentation for entities

--- fs/fs.go
+++ fs/fs.go
@@ -1,3 +1,5 @@
+// Package fs provides a variety of filesystem utilities. It
+// is Espresso's interface for interacting with the filesystem.
 package fs
 
 import (
@@ -8,14 +10,20 @@ import (
 )
 
 var (
+	// MarkdownOnly is a predefined filter that only allows
+	// .md files.
 	MarkdownOnly = func(file string) bool {
 		return filepath.Ext(file) == ".md"
 	}
+	// IgnoreUnderscores is a filter that doesn't let pass
+	// files starting with an underscore.
 	IgnoreUnderscores = func(file string) bool {
 		return !strings.HasPrefix(filepath.Base(file), "_")
 	}
 )
 
+// StreamFiles streams all files in `path` that match all `filters`
+// by sending them through the `files` channel.
 func StreamFiles(path string, files chan<- string, filters ...func(file string) bool) error {
 	err := filepath.
 		Walk(path, func(file string, info os.FileInfo, err error) error {
@@ -35,6 +43,7 @@ func StreamFiles(path string, files chan<- string, filters ...func(file string) 
 	return err
 }
 
+// CreateDir creates a directory specified in `path`.
 func CreateDir(path string, createParents bool) error {
 	if createParents {
 		return os.MkdirAll(path, 0700)
@@ -42,10 +51,12 @@ func CreateDir(path string, createParents bool) error {
 	return os.Mkdir(path, 0700)
 }
 
+// CreateFile creates a file with the given filename.
 func CreateFile(file string) (*os.File, error) {
 	return os.Create(file)
 }
 
+// CopyDir copies a directory to `dest` recursively.
 func CopyDir(source, dest string) error {
 	return copy.Copy(source, dest)
 }
