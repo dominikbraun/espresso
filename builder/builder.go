d614f67fd3c0f863aa5a85746aeeacfd96cafd71
Dominik Braun <mail@dominikbraun.io>
2020-08-04 12:30:04 +0200

    Replace route map with route tree

--- builder/builder.go
+++ builder/builder.go
@@ -21,47 +21,14 @@ type builder struct {
 }
 
 func (b *builder) RegisterPage(route string, page entity.Page) error {
-	if _, exists := b.site.Routes[route]; !exists {
-		r := entity.Route{
-			Pages:     make([]entity.Page, 0),
-			IndexPage: entity.IndexPage{},
-		}
-		b.site.Routes[route] = &r
-	}
+	r := b.site.CreateRoute(route)
 
-	b.site.Routes[route].Pages = append(b.site.Routes[route].Pages, page)
-	b.site.Routes[route].IndexPage.Pages = append(b.site.Routes[route].IndexPage.Pages, page)
+	r.Pages = append(r.Pages, page)
+	r.IndexPage.Pages = append(r.IndexPage.Pages, page)
 
 	return nil
 }
 
 func (b *builder) Dispatch() (entity.Site, error) {
-	var (
-		site entity.Site
-	)
-
-	if err := b.resolveRelated(); err != nil {
-		return site, err
-	}
-
-	return site, nil
-}
-
-func (b *builder) resolveRelated() error {
-	err := b.site.
-		WalkRoutes(func(route *entity.Route) error {
-			for _, page := range route.Pages {
-				for _, fqn := range page.RelatedFQNs {
-
-					p, err := b.site.ResolvePage(fqn)
-					if err != nil {
-						return err
-					}
-					page.Related = append(page.Related, p)
-				}
-			}
-			return nil
-		})
-
-	return err
+	return b.site, nil
 }
--- entity/site.go
+++ entity/site.go
@@ -2,7 +2,10 @@
 // used to represent the entire website and its components.
 package entity
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Site represents the actual website and is thus the root entity
 // which contains all website components.
@@ -11,39 +14,104 @@ import "fmt"
 // entity which can be modified by plugins and used for rendering
 // the website files.
 type Site struct {
-	Meta   Meta              // Meta contains page-agnostic, global metadata for the website itself.
-	Nav    Nav               // Nav contains the global website navigation.
-	Routes map[string]*Route // Routes contains all routes and their pages.
-	Footer Footer            // Footer contains the global website footer.
+	Meta   Meta   // Meta contains page-agnostic, global metadata for the website itself.
+	Nav    Nav    // Nav contains the global website navigation.
+	Root   Route  // Root is the root element of the route tree.
+	Footer Footer // Footer contains the global website footer.
 }
 
-// WalkRoutes invokes a function on each registered route.
-func (s Site) WalkRoutes(walkFn func(route *Route) error) error {
-	for _, route := range s.Routes {
-		if err := walkFn(route); err != nil {
+// WalkRoutes walks down the route tree recursively and invokes a
+// function on each route. If this function returns an error, the
+// error will be propagated back to the caller.
+//
+// WalkRoutes will walk down the route tree to the desired depth.
+// Set `maxDepth` to `-1` to disable this limitation.
+func (s *Site) WalkRoutes(walkFn func(route *Route) error, maxDepth int) error {
+	return s.walkRoute(&s.Root, walkFn, maxDepth, 0)
+}
+
+// walkRoute is the internal implementation of WalkRoutes which
+// calls the walk function on a given route and then calls itself
+// for each of the route's children.
+func (s *Site) walkRoute(route *Route, walkFn func(route *Route) error, maxDepth, curDepth int) error {
+	if maxDepth != -1 && curDepth == maxDepth {
+		return nil
+	}
+	curDepth++
+
+	if err := walkFn(route); err != nil {
+		return err
+	}
+
+	for _, route := range route.Children {
+		if err := s.walkRoute(route, walkFn, maxDepth, curDepth); err != nil {
 			return err
 		}
 	}
+
 	return nil
+}
+
+// ResolveRoute resolves a route in the route tree and returns
+// it. Returns an error if the route cannot be found.
+func (s *Site) ResolveRoute(route string) (*Route, error) {
+	var (
+		node     = &s.Root
+		segments = strings.Split(route, "/")
+	)
+
+	for i, s := range segments {
+		if i == len(segments)-1 {
+			return node, nil
+		}
+		if _, exists := node.Children[s]; !exists {
+			return nil, fmt.Errorf("child %s does not exist", s)
+		}
+		node = node.Children[s]
+	}
+
+	return nil, fmt.Errorf("route %s does not exist", route)
 }
 
 // ResolvePage resolves a FQN to a page in the route list.
 func (s *Site) ResolvePage(fqn FQN) (Page, error) {
-	var (
-		page   Page
-		route  = fqn.Route()
-		pageID = fqn.PageID()
-	)
+	route, err := s.
+		ResolveRoute(fqn.Route())
 
-	if _, exists := s.Routes[route]; !exists {
-		return page, fmt.Errorf("related page %s: route %s does not exist", fqn.String(), route)
+	if err != nil {
+		return Page{}, err
 	}
 
-	for _, p := range s.Routes[route].Pages {
-		if p.ID == pageID {
-			return page, nil
+	for _, p := range route.Pages {
+		if p.ID == fqn.PageID() {
+			return p, nil
 		}
 	}
 
-	return page, fmt.Errorf("related page %s not found", fqn.String())
+	return Page{}, fmt.Errorf("related page %s not found", fqn.String())
+}
+
+// CreateRoute creates a route in the route tree along with its
+// parents and returns that route.
+func (s *Site) CreateRoute(route string) *Route {
+	var (
+		node     = &s.Root
+		segments = strings.Split(route, "/")
+	)
+
+	for i, s := range segments {
+		if _, exists := node.Children[s]; !exists {
+			node.Children[s] = &Route{
+				Children:  make(map[string]*Route),
+				Pages:     make([]Page, 0),
+				IndexPage: IndexPage{},
+			}
+		}
+		if i == len(segments)-1 {
+			return node
+		}
+		node = node.Children[s]
+	}
+
+	return nil
 }
